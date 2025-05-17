89529df00e87e34b2de75804ee540c91ce10b225
Lime104682 <lime030104@naver.com>
2025-05-17 21:09:14 +0900

    Task02_Pooling 파일 삭제, Task02~03 생성

---
+++ Task02~03/Assets/Task03_Json.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+[System.Serializable]
+public class MonsterState
+{
+    public float attack;
+    public float defense;
+    public int maxHP;
+}
+
+[System.Serializable]
+public class Monster
+{
+    public int ID;
+    public string name;
+    public MonsterState state;
+}
+
+[System.Serializable]
+public class MonsterList
+{
+    public List<Monster> monsters;
+}
+
+public class Task03_Json : MonoBehaviour
+{
+    void Start()
+    {
+        string path = Application.dataPath + "/Task03_DataTable.csv";
+
+        string[] lines = File.ReadAllLines(path);
+
+        List<Monster> monsters = new List<Monster>();
+        for (int i = 1; i < lines.Length; i++)
+        {
+            string[] parts = lines[i].Split(',');
+
+            Monster m = new Monster
+            {
+                ID = int.Parse(parts[0]),
+                name = parts[1],
+                state = new MonsterState
+                {
+                    attack = float.Parse(parts[2]),
+                    defense = float.Parse(parts[3]),
+                    maxHP = int.Parse(parts[4])
+                }
+            };
+            monsters.Add(m);
+        }
+
+        string json = JsonUtility.ToJson(new Wrapper<Monster> { items = monsters.ToArray() }, true);
+        Debug.Log(json);
+    }
+
+    [System.Serializable]
+    private class Wrapper<T>
+    {
+        public T[] items;
+    }
+}
